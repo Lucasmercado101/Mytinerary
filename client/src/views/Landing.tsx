5c696a6f5a1ceadb0eb758d8a5910936ccdb615c
Lucas <lucasmercado101@gmail.com>
2020-10-17 10:24:35 -0300

    made the landing somewhat responsive

--- client/src/views/Landing.tsx
+++ client/src/views/Landing.tsx
@@ -1,56 +1,205 @@
 import React, { useEffect } from "react";
 import mytineraryLogo from "../Images/mytinerary_logo.svg";
-import circleright from "../Images/arrowRight.svg";
+import {
+  Grid,
+  makeStyles,
+  Button,
+  Typography,
+  Container,
+  useTheme,
+  useMediaQuery
+} from "@material-ui/core";
 import { Link } from "react-router-dom";
-// import MyLink from "../MyLink";
-// import carousel1img from "../../Images/carousel1.jpg";
-// import carousel2img from "../../Images/carousel2.jpg";
-// import carousel3img from "../../Images/carousel3.jpg";
-// import carousel4img from "../../Images/carousel4.jpg";
-// import carousel5img from "../../Images/carousel5.jpg";
+import HeroImg from "../Images/hero.svg";
 // import Slider from "react-animated-slider";
-// import "react-animated-slider/build/horizontal.css";
 
-// const images = [
-//   carousel1img,
-//   carousel2img,
-//   carousel3img,
-//   carousel4img,
-//   carousel5img,
-// ];
+const useStyles = makeStyles(() => ({
+  landingPage: {
+    height: "100%",
+    width: "100%"
+  },
+  title: {
+    textTransform: "capitalize"
+  },
+  landingSection: {
+    width: "100%",
+    height: "100%",
+    padding: 25,
+    margin: 0
+  },
+  logo: {
+    width: "100%",
+    display: "block",
+    margin: "0 auto"
+  },
+  image: {
+    width: "100%"
+  },
+  button: {
+    width: "100%"
+  }
+}));
 
 function Landing() {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+  const {
+    landingPage,
+    logo,
+    title,
+    button,
+    landingSection,
+    image
+  } = useStyles();
+
   useEffect(() => {
     document.title = "Mytinerary";
   }, []);
-
+  //TODO: fix this
   return (
-    <>
-      <header>
-        <img alt={"Mytinerary log"} src={mytineraryLogo} />
-        <h1>
-          Find your perfect trip, designed by insiders who know and love their
-          cities.
-        </h1>
-        <Link to="/cities">
-          <img alt={"White right arrow"} src={circleright} />
-        </Link>
-      </header>
-      <section>
-        <h2>Popular MYtineraries</h2>
-
-        {/* <Slider style={{ backgroundColor: "red" }} autoplay={3000}>
-          {images.map((item, index) => (
-            <div
-              key={index}
-              style={{
-                background: `url('${item}') no-repeat center center / cover`,
-              }}
-            ></div>
-          ))}
-        </Slider> */}
-      </section>
-    </>
+    <Container maxWidth="xl" className={landingPage}>
+      {matches ? (
+        <Grid style={{ height: "100%" }} container direction="row">
+          <Grid
+            xs={6}
+            container
+            spacing={6}
+            wrap="nowrap"
+            justify="center"
+            direction="column"
+            className={landingSection}
+            component="section"
+          >
+            <Grid item>
+              <img
+                className={image}
+                alt={"Mytinerary log"}
+                src={mytineraryLogo}
+              />
+            </Grid>
+            <Grid item>
+              <Typography
+                className={title}
+                align="left"
+                variant="h3"
+                color="primary"
+                component="h1"
+              >
+                Find your perfect trip
+              </Typography>
+              <Typography
+                align="left"
+                variant="h4"
+                color="textSecondary"
+                component="h2"
+              >
+                Designed by insiders who know and love their cities.
+              </Typography>
+            </Grid>
+            <Grid
+              spacing={1}
+              direction="row"
+              justify="flex-start"
+              container
+              item
+            >
+              <Grid item>
+                <Link to="/cities">
+                  <Button variant="text" size="large" color="primary">
+                    browse cities
+                  </Button>
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to="/register">
+                  <Button size="large" variant="contained" color="primary">
+                    sign up
+                  </Button>
+                </Link>
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid xs={6} alignItems="center" container item>
+            <Grid xs={11} item>
+              <img
+                className={image}
+                src={HeroImg}
+                alt="person next to a globe"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      ) : (
+        <Grid
+          container
+          spacing={2}
+          wrap="nowrap"
+          justify="space-between"
+          direction="column"
+          className={landingSection}
+          component="section"
+        >
+          <Grid xs item>
+            <img
+              className={image}
+              alt={"Mytinerary log"}
+              src={mytineraryLogo}
+            />
+          </Grid>
+          <Grid xs justify="center" container item>
+            <Grid xs={11} item>
+              <img
+                className={image}
+                src={HeroImg}
+                alt="person next to a globe"
+              />
+            </Grid>
+          </Grid>
+          <Grid xs item>
+            <Typography
+              className={title}
+              align="center"
+              variant="h5"
+              color="primary"
+              component="h1"
+            >
+              Find your perfect trip
+            </Typography>
+            <Typography
+              align="center"
+              variant="subtitle1"
+              color="textSecondary"
+              component="h2"
+            >
+              Designed by insiders who know and love their cities.
+            </Typography>
+          </Grid>
+          <Grid
+            xs
+            spacing={1}
+            direction="column"
+            justify="space-between"
+            container
+            item
+          >
+            <Grid item>
+              <Link to="/register">
+                <Button className={button} variant="contained" color="primary">
+                  sign up
+                </Button>
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link to="/cities">
+                <Button variant="text" className={button} color="primary">
+                  browse cities
+                </Button>
+              </Link>
+            </Grid>
+          </Grid>
+        </Grid>
+      )}
+    </Container>
   );
 }
 
