3f14a9ad50f3fd8da70b2c9c5d8aebec81c16339
Lucasmercado101 <lucasmercado101@gmail.com>
2021-10-28 15:27:44 -0300

    added city and login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Cities from "./pages/Cities";
+import City from "./pages/City";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={Landing} />
       <Route exact path="/cities" component={Cities} />
+      <Route exact path="/cities/:id" component={City} />
+      <Route exact path="/login" component={Login} />
     </div>
   );
 }
