7a416d1e13c59befb41aef78318c0b9051352a8d
Lucasmercado101 <lucasmercado101@gmail.com>
2021-10-28 16:52:18 -0300

    checks if user is logged in

--- src/pages/Cities.tsx
+++ src/pages/Cities.tsx
@@ -1,39 +1,70 @@
-import { CityResp, createCity, getCities } from "../api";
+import {
+  CityResp,
+  createCity,
+  getCities,
+  isLoggedIn as isLoggedInQuery
+} from "../api";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Cities() {
   const { data: citiesData, isFetched } = useQuery("cities", getCities);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useQuery("user", isLoggedInQuery, {
+    staleTime: 0,
+    onSuccess: () => {
+      setIsLoggedIn(true);
+    }
+  });
   const { mutateAsync } = useMutation(createCity);
   const [newCityName, setNewCityName] = useState("");
+  const [newCountry, setNewCountry] = useState("");
   const client = useQueryClient();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!newCityName || !newCountry) return;
+
+    mutateAsync({
+      name: newCityName,
+      country: newCountry
+    }).then(() => {
+      setNewCityName("");
+      client.invalidateQueries("cities");
+    });
+  };
 
   return (
     <div>
       Cities:
-      <input
-        value={newCityName}
-        onChange={(e) => setNewCityName(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          mutateAsync({ name: newCityName }).then(() => {
-            setNewCityName("");
-            client.invalidateQueries("cities");
-          });
-        }}
-      >
-        Create city
-      </button>
-      {isFetched &&
-        citiesData &&
-        citiesData.data.map((city: CityResp) => (
-          <div key={city.id}>
-            {city.name}
-            <Link to={`/cities/${city.id}`}>Go to city</Link>
-          </div>
-        ))}
+      {isLoggedIn && (
+        <form onSubmit={handleSubmit}>
+          <input
+            value={newCityName}
+            onChange={(e) => setNewCityName(e.target.value)}
+          />
+          <input
+            value={newCountry}
+            onChange={(e) => setNewCountry(e.target.value)}
+          />
+          <button>Create city</button>
+        </form>
+      )}
+      {isFetched && citiesData && (
+        <ul>
+          {citiesData.data.map((city: CityResp) => (
+            <li key={city.id}>
+              <div>
+                {city.name}
+                <br />
+                {city.country}
+              </div>
+              <Link to={`/cities/${city.id}`}>Go to city</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
