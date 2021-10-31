c0bc04c8a69198933c98c7907e910371002619e6
Lucasmercado101 <lucasmercado101@gmail.com>
2021-10-31 20:27:28 -0300

    added landing styles

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,61 @@
-import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
+import { useHistory } from "react-router-dom";
+import arrowRight from "../assets/arrowRight.svg";
+import mytinerary_logo from "../assets/mytinerary_logo.svg";
+import heroBgr from "../assets/heroBgr.jpg";
 
 function Landing() {
+  const history = useHistory();
+
   return (
     <div>
-      Landing Page
-      <Link to="/cities">Cities list</Link>
+      <section
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+          alignItems: "center",
+          height: "calc(100vh - 56px)",
+          width: "100vw"
+        }}
+      >
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            objectPosition: "center",
+            position: "absolute",
+            top: 0,
+            left: 0,
+            zIndex: -1,
+            filter: "brightness(0.5)"
+          }}
+          src={heroBgr}
+          alt="hero"
+        />
+        <img
+          style={{
+            width: "90%"
+          }}
+          src={mytinerary_logo}
+          alt="mytinerary logo"
+        />
+        <Typography textAlign="center" variant="h4" component="h1">
+          Find your perfect trip, designed by insiders who know and love their
+          cities.
+        </Typography>
+        <img
+          onClick={() => history.push("/cities")}
+          style={{
+            width: "45%",
+            maxWidth: 300,
+            cursor: "pointer"
+          }}
+          src={arrowRight}
+          alt="arrow"
+        />
+      </section>
     </div>
   );
 }
