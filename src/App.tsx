91db4733423e605eb4386916bd6fb9cc0d344038
Lucasmercado101 <lucasmercado101@gmail.com>
2021-10-31 20:07:37 -0300

    added theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import Cities from "./pages/Cities";
 import City from "./pages/City";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ThemeProvider from "./Theme";
 
 function App() {
   return (
-    <div>
+    <ThemeProvider>
       <Route exact path="/" component={Landing} />
       <Route exact path="/cities" component={Cities} />
       <Route exact path="/cities/:id" component={City} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
-    </div>
+    </ThemeProvider>
   );
 }
 
