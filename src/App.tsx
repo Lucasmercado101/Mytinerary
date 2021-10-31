b15eb66bcbb3762ae97158b2e683bc93e88ccdc4
Lucasmercado101 <lucasmercado101@gmail.com>
2021-10-31 20:27:34 -0300

    added toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,26 @@ import City from "./pages/City";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ThemeProvider from "./Theme";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 function App() {
   return (
     <ThemeProvider>
+      <AppBar position="static">
+        <Toolbar>
+          {/* <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton> */}
+          {/* <Typography variant="h6" color="inherit" component="div">
+            Photos
+          </Typography> */}
+        </Toolbar>
+      </AppBar>
       <Route exact path="/" component={Landing} />
       <Route exact path="/cities" component={Cities} />
       <Route exact path="/cities/:id" component={City} />
