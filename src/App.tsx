8811608c086174c6b5fbd1ee4fbafdaa5d1dcd75
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-01 10:29:51 -0300

    added logged out user menu to app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,75 @@
-import { Route } from "react-router-dom";
+import { useState } from "react";
+import { Route, useHistory } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Cities from "./pages/Cities";
 import City from "./pages/City";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ThemeProvider from "./Theme";
-import { AppBar, Avatar, IconButton, Toolbar, Typography } from "@mui/material";
-import { Box } from ".pnpm/@mui+system@5.0.6_0c6b44af47723f3fbfad0689dde655a8/node_modules/@mui/system";
+import {
+  AppBar,
+  Avatar,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar
+} from "@mui/material";
+import { Box } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useQuery } from "react-query";
+import { isLoggedIn as isLoggedInQuery } from "./api";
 
 function App() {
+  const history = useHistory();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useQuery("user", isLoggedInQuery, {
+    staleTime: 0,
+    onSuccess: () => {
+      setIsLoggedIn(true);
+    }
+  });
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    setIsMenuOpen(false);
+  };
+
+  const renderLoggedOutMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right"
+      }}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right"
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem
+        onClick={() => {
+          history.push("/login");
+          handleMenuClose();
+        }}
+      >
+        Log In
+      </MenuItem>
+      <MenuItem
+        onClick={() => {
+          history.push("/register");
+          handleMenuClose();
+        }}
+      >
+        Register Account
+      </MenuItem>
+    </Menu>
+  );
+
   return (
     <ThemeProvider>
       <AppBar position="sticky">
@@ -18,7 +78,12 @@ function App() {
             <MenuIcon />
           </IconButton>
           <Box sx={{ flexGrow: 1 }} />
-          <IconButton size="large" edge="end" color="inherit">
+          <IconButton
+            onClick={() => setIsMenuOpen(true)}
+            size="large"
+            edge="end"
+            color="inherit"
+          >
             <Avatar />
           </IconButton>
         </Toolbar>
@@ -28,6 +93,7 @@ function App() {
       <Route exact path="/cities/:id" component={City} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
+      {renderLoggedOutMenu}
     </ThemeProvider>
   );
 }
