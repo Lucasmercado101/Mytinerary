b5b4f7f959e03dac521a1c214928917d7f5156c7
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-01 10:19:20 -0300

    added icons to app bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,22 @@ import City from "./pages/City";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ThemeProvider from "./Theme";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Avatar, IconButton, Toolbar, Typography } from "@mui/material";
+import { Box } from ".pnpm/@mui+system@5.0.6_0c6b44af47723f3fbfad0689dde655a8/node_modules/@mui/system";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function App() {
   return (
     <ThemeProvider>
       <AppBar position="sticky">
         <Toolbar>
-          {/* <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
             <MenuIcon />
-          </IconButton> */}
-          {/* <Typography variant="h6" color="inherit" component="div">
-            Photos
-          </Typography> */}
+          </IconButton>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton size="large" edge="end" color="inherit">
+            <Avatar />
+          </IconButton>
         </Toolbar>
       </AppBar>
       <Route exact path="/" component={Landing} />
