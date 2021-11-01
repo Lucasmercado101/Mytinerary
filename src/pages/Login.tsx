5216e70ad8705e3f4c87a4a41627b4bceea5dc71
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-01 10:51:51 -0300

    added styles and some error messages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,31 +1,51 @@
+import { Box } from ".pnpm/@mui+system@5.0.6_0c6b44af47723f3fbfad0689dde655a8/node_modules/@mui/system";
+import { Alert, Button, TextField } from "@mui/material";
 import { useState } from "react";
+import { useMutation } from "react-query";
 import { login } from "../api";
 
 function Login() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const { mutate, error, isError } = useMutation(login);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    login({ username: userName, password });
+    mutate({ username: userName, password });
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={userName}
-          onChange={(e) => setUserName(e.target.value)}
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button>Login</button>
-      </form>
-    </div>
+    <Box
+      onSubmit={handleSubmit}
+      component="form"
+      m={2}
+      display="flex"
+      flexDirection="column"
+      gap={3}
+    >
+      <TextField
+        label="Username"
+        type="text"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button variant="contained" type="submit">
+        Login
+      </Button>
+      {isError && (
+        <Alert severity="error">
+          {(error as any).response.status === 404
+            ? "Invalid username or password"
+            : "An Error has occurred on the server, please try again."}
+        </Alert>
+      )}
+    </Box>
   );
 }
 
