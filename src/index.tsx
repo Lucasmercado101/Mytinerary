5240b08036d11cde8d1896108f25378814cfd6a7
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-01 12:08:54 -0300

    added context provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "react-query";
-
+import ContextProvider from "./Context";
 const client = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <QueryClientProvider client={client}>
-        <App />
+        <ContextProvider>
+          <App />
+        </ContextProvider>
       </QueryClientProvider>
     </Router>
   </React.StrictMode>,
