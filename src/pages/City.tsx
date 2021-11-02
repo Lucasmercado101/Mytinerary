d8e4fd0e2fcc4a7a868aac36c3674b7c97d4fa70
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-02 13:04:06 -0300

    reduced img opacity

--- src/pages/City.tsx
+++ src/pages/City.tsx
@@ -35,7 +35,7 @@ function City() {
               width: "100%",
               height: "100%",
               zIndex: -1,
-              opacity: 0.5
+              opacity: 0.4
             }}
           />
           <Box
