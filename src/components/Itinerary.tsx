0078240b2030b459c73337d3c1231a9ca527f2a7
Lucasmercado101 <lucasmercado101@gmail.com>
2021-11-03 14:09:55 -0300

    added styles

--- src/components/Itinerary.tsx
+++ src/components/Itinerary.tsx
@@ -13,45 +13,59 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { CityItinerariesResponse } from "../api";
+import MoneyIcon from "@mui/icons-material/AttachMoney";
+import ClockIcon from "@mui/icons-material/QueryBuilder";
+import { Grid, Box, Chip } from "@mui/material";
 
-const Itinerary: React.FC = (props) => {
+const Itinerary: React.FC<{ data: CityItinerariesResponse }> = ({ data }) => {
+  const { activities, creator, hashtags, price, time, title } = data;
+  const { userId, profilePic } = creator;
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ width: "100%", backgroundColor: "paper" }}>
       <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
+        avatar={<Avatar src={profilePic} />}
+        // action={
+        //   <IconButton aria-label="settings">
+        //     <MoreVertIcon />
+        //   </IconButton>
+        // }
+        title={<Typography>{title}</Typography>}
       />
       <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
-        </Typography>
+        <Grid container spacing={3}>
+          <Grid container item spacing={1} mx={1}>
+            <Grid item xs={6}>
+              <Box display="flex" gap={0.5} alignItems="center">
+                <ClockIcon />
+                <Typography>{time}</Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <Box display="flex" gap={0} alignItems="center">
+                <MoneyIcon />
+                <Typography>{price}</Typography>
+              </Box>
+            </Grid>
+          </Grid>
+          <Grid container item spacing={1} mx={0}>
+            {hashtags.map((hashtag, i) => (
+              <Grid key={i} item xs={3}>
+                <Chip label={`#${hashtag}`} variant="filled" />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
       </CardContent>
-      <CardActions disableSpacing>
+      {/* <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
           <FavoriteIcon />
         </IconButton>
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
-      </CardActions>
+      </CardActions> */}
     </Card>
   );
 };
